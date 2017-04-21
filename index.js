a04094ac6c465189cf963eb31c7ae74a9de59143
Ahmad Raza Khan <ahmad.khan@gmail.com>
2017-04-21 16:48:11 -0700

    changed the license and added NOTICE.txt

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 /******************
+ *
+ * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
  * @author Ahmad R. Khan
  *
  * An AWS Lambda function that integrates Twilio Programmable SMS with Amazon Lex.
- * [Twilio SMS]<--->[API Gateway]<--->[this lambda function]<--->[Lex]
+ * [Twilio SMS]<--->[API Gateway]<--->[this lambda function]<--->[Amazon Lex]
  ******************/
 
 var twilio = require('twilio');
@@ -53,7 +61,7 @@ exports.handler = (event, context, callback) => {
 			      twimlResponse.message('Sorry, we ran into a problem at our end.');
 			      callback(err, twimlResponse.toString());
 					} else {
-						console.log(data);           // got something back from Lex
+						console.log(data);           // got something back from Amazon Lex
 						twimlResponse.message(data.message);
 		        callback(null, twimlResponse.toString());
 					}
